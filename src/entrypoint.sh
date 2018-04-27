4f69eb7d98b577f55794dadc6e3850e86e7e6029
Mediaviya <35331832+Mediaviya@users.noreply.github.com>
2018-04-27 13:07:59 +0200

    Add files via upload

---
+++ src/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+Entorno=$(aws ssm get-parameters --names $(echo $TYPE).Entorno --no-with-decryption --region eu-west-1 --query "Parameters[*].{Name:Value}" --output text)
+
+echo $Entorno > /var/www/info.php
+
+/usr/sbin/apache2 -D FOREGROUND
