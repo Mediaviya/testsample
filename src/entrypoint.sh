87aa01b4232f5cb54a16fd90c63b2fb2712adc11
Mediaviya <35331832+Mediaviya@users.noreply.github.com>
2018-04-30 10:28:07 +0200

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-Entorno=$(aws ssm get-parameters --names $(echo $TYPE).Entorno --no-with-decryption --region eu-west-1 --query "Parameters[*].{Name:Value}" --output text)
+#Entorno=$(aws ssm get-parameters --names $(echo $TYPE).Entorno --no-with-decryption --region eu-west-1 --query "Parameters[*].{Name:Value}" --output text)
 
-echo $Entorno > /var/www/info.php
+#echo $Entorno > /var/www/info.php
 
 /usr/sbin/apache2 -DFOREGROUND
