04ea2a4bf47ae9ac2922f1a9a39ed35dc738351e
Mediaviya <35331832+Mediaviya@users.noreply.github.com>
2018-05-03 11:53:47 +0200

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+update-ca-trust force-enable
+update-ca-trust extract
 Entorno=$(/usr/bin/aws ssm get-parameters --names $(echo $TYPE).Entorno --no-with-decryption --region eu-west-1 --query "Parameters[*].{Name:Value}" --output text)
 echo $Entorno > /var/www/info.php
 #/usr/sbin/apache2 -DFOREGROUND
