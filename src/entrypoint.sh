955ea06caefb93ea6280ace0e82cd103ffb17876
Mediaviya <35331832+Mediaviya@users.noreply.github.com>
2018-05-03 16:01:00 +0200

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 Entorno=$(/root/.local/bin/aws ssm get-parameters --names $(echo $TYPE).Entorno --no-with-decryption --region eu-west-1 --query "Parameters[*].{Name:Value}" --output text)
-echo $Entorno > "/var/www/html/info/info.php"
-cp /var/www/html/info/info.php? /var/www/html/info/info.php
+echo $Entorno > "/var/www/html/info.php"
+mkdir /var/www/html/info
+cp /var/www/html/info.php? /var/www/html/info/info.php
 #/usr/sbin/apache2 -DFOREGROUND
 exec "$@";
