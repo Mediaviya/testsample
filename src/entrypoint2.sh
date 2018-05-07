c75772b9f6aa6cfc2249f12a1914731233b73145
Mediaviya <35331832+Mediaviya@users.noreply.github.com>
2018-05-07 10:57:39 +0200

    Delete entrypoint2.sh

--- src/entrypoint2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-Entorno=$(/root/.local/bin/aws ssm get-parameters --names $(echo $TYPE).Entorno --no-with-decryption --region eu-west-1 --query "Parameters[*].{Name:Value}" --output text)
-echo $Entorno > '/var/www/html/info.php'
-#/usr/sbin/apache2 -DFOREGROUND
-exec "$@";
