34aaa506315c89d3b5c9979c3d0d508af06042f5
Mediaviya <mediaviya@hotmail.com>
2018-06-29 09:20:40 +0000

    apache

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-Entorno=$(/root/.local/bin/aws ssm get-parameters --names $(echo $TYPE).Entorno --no-with-decryption --region eu-west-1 --query "Parameters[*].{Name:Value}" --output text) && echo $Entorno > '/var/www/html/info.php' && ping localhost;
+Entorno=$(/root/.local/bin/aws ssm get-parameters --names $(echo $TYPE).Entorno --no-with-decryption --region eu-west-1 --query "Parameters[*].{Name:Value}" --output text) && echo $Entorno > '/var/www/html/info.php' && service httpd start && ping localhost;
